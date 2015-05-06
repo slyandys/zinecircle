39887ef8f8a9bd3bd730e114aff30bcede28980c
Summer Xia <zxia010@uottawa.ca>
2015-05-06 14:42:47 -0400

    change gas price
    use another website and xpath to get the gas price in Ottawa.

--- gas.php
+++ gas.php
@@ -1,26 +1,25 @@
 <?php
 function getGas()
 {
-	//data from GasBuddy.com
-	$link = "http://www.gasbuddy.com/GB_Price_List.aspx?cntry=CAN#can_cities";
-
+	//data source
+	$link = "http://www.ottawagasprices.com/";
+	
+	//locate
 	$html = file_get_contents($link);
+	//echo $html;
 	$doc = new DOMDocument();
 	$doc->loadHTML($html);
+	//echo $doc->saveHTML();
 	$xpath = new DOMXPath($doc);
-	$tables = $doc->getElementsByTagName('table')->item(2);
-	
-	//Ottawa
-	$nodes  = $xpath->query('.//tbody/tr/td/a', $tables);
-	//var_dump($nodes->item(6)->nodeValue);
-	
-	//Gas price
-	//$nodes  = $xpath->query('.//tbody[1]/tr[7]/td', $tables->item(2));
-	$nodes  = $xpath->query('.//tr/td/a[contains(text(),"Ottawa")]', $tables);
+	//$elements = $xpath->query("//*[@id]");
+	//echo $elements;
+	//$tables = $doc->getElementsByTagName('table');
+	$nodes  = $xpath->query("//table[@class='SideAvg']/tr/td");
+	//print_r($nodes);
 	//var_dump($nodes->item(1)->nodeValue);
-	print $nodes->item(1)->nodeValue;
 	return $nodes->item(1)->nodeValue;
 }
+
 //Call
 $_G['Gas'] = getGas();
 ?>
