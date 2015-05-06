b47b83102c5c95575f71577732cb5cd944cee467
Summer Xia <zxia010@uottawa.ca>
2015-05-06 17:15:54 -0400

    change weather API
    source from http://weather.gc.ca/city/pages/on-118_metric_e.html
    instead of openmap since it has large deviation

--- weather.php
+++ weather.php
@@ -4,23 +4,26 @@ function getWeather()
 {
 	$city="Ottawa"; //set city
 	$country="CA"; //set country
-	$url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
-	$json=file_get_contents($url);
-	$data=json_decode($json,true);
+	//$url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
 	
-	//Examples
-	//Get current Temperature in Celsius
-	//echo $data['main']['temp']."&#176;C"."<br>";
-	//Get weather condition
-	//echo $data['weather'][0]['main']."<br>";
-	//Get cloud percentage
-	//echo $data['clouds']['all']."<br>";
-	//Get wind speed
-	//echo $data['wind']['speed']."<br>";
+	$url="http://weather.gc.ca/city/pages/on-118_metric_e.html";
 	
-	return $data['weather'][0]['main']."&nbsp&nbsp".$data['main']['temp']."&#176;C";
+	
+	$html=file_get_contents($url);
+	
+	$doc = new DOMDocument();
+	$doc->loadHTML($html);
+	//echo $doc->saveHTML();
+	$xpath = new DOMXPath($doc);
+	//$elements = $xpath->query("//*[@id]");
+	//echo $elements;
+	//$tables = $doc->getElementsByTagName('table');
+	$nodes  = $xpath->query("//*[@id='currconditionscontainer']/div[3]/div/div[1]/div/div[2]/p");
+	//print_r($nodes);
+	//var_dump($nodes->item(0)->nodeValue);
+	return $nodes->item(0)->nodeValue;
 }
-
+ 
 //Call
 $_G['weather'] = getWeather();
 
